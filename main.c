4ce6808126306df7b23988fe6a37b4a81f10e0f4
GabsRMartins <gabsmartins262@gmail.com>
2024-03-16 19:49:06 -0300

    feature:calculos em rad

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include "libmatematica.h"
+
+int main() {
+     float anguloRad;
+    printf("Digite o angulo em radianos\n");
+    scanf("%f",&anguloRad);
+    float seno = calcular_seno(anguloRad);
+    printf("Seno de %.2f radianos: %.6f\n", anguloRad, seno);
+    float cosseno = calcular_cosseno(anguloRad);
+    printf("Cosseno de %.2f radianos: %.6f\n", anguloRad, cosseno);
+    float tangente = calcular_tangente(anguloRad);
+    printf("Tangente de %.2f radianos: %.6f\n", anguloRad, tangente);
+
+    return 0;
+}
